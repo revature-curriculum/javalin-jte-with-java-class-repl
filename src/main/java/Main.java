3f7e6d0844ddc3545cc32a7a0663821285860e16
Ashoka Shringla <ashokashringla@gmail.com>
2022-02-02 16:25:26 +0000

    readme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,24 @@ public class Main {
 
     static ArrayList<ShoppingItem> items = new ArrayList<>();
 
+    //TO-DO create array
+
     public static void main(String[] args) {
 
 
         // Adding new ShoppingItems to the list
         items.add(new ShoppingItem("Shirt", 30.0f));
         items.add(new ShoppingItem("Pants", 35.0f));
+
+        // TO-DO add to your array
         
         Javalin app = Javalin.create().start(4100);
 
         app.get("/", ctx -> ctx.render("index.jte"));
 
         app.get("/shoppingList", shoppingHandler);
+
+        // TO-DO create route
 
     }
 
@@ -28,5 +34,6 @@ public class Main {
 
     };
 
+  // TO-DO create handler
 
 }
